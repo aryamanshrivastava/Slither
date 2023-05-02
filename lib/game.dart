510830b7e727ede3fb78e95687b618a9329a0409
Aryaman Shrivastava <as8727@srmist.edu.in>
2023-05-02 15:22:34 +0530

    color n alignment

--- lib/game.dart
+++ lib/game.dart
@@ -14,7 +14,6 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> with TickerProviderStateMixin {
-
   late int _playerScore;
   late bool _hasStarted;
   late Animation<double> _snakeAnimation;
@@ -39,15 +38,16 @@ class _GamePageState extends State<GamePage> with TickerProviderStateMixin {
     _hasStarted = true;
     do {
       _snakeFoodPosition = _random.nextInt(_noOfSquares);
-    } while(_snake.contains(_snakeFoodPosition));
+    } while (_snake.contains(_snakeFoodPosition));
     _snakeController = AnimationController(vsync: this, duration: _duration);
-    _snakeAnimation = CurvedAnimation(curve: Curves.easeInOut, parent: _snakeController);
+    _snakeAnimation =
+        CurvedAnimation(curve: Curves.easeInOut, parent: _snakeController);
   }
 
   void _gameStart() {
     Timer.periodic(Duration(milliseconds: 250), (Timer timer) {
       _updateSnake();
-      if(_hasStarted) timer.cancel();
+      if (_hasStarted) timer.cancel();
     });
   }
 
@@ -59,20 +59,22 @@ class _GamePageState extends State<GamePage> with TickerProviderStateMixin {
   }
 
   void _updateSnake() {
-    if(!_hasStarted) {
+    if (!_hasStarted) {
       setState(() {
         _playerScore = (_snake.length - 4) * 100;
         switch (_currentSnakeDirection) {
           case 'DOWN':
             if (_snake.last > _noOfSquares) {
-              _snake.add(_snake.last + _squareSize - (_noOfSquares + _squareSize));
+              _snake.add(
+                  _snake.last + _squareSize - (_noOfSquares + _squareSize));
             } else {
               _snake.add(_snake.last + _squareSize);
             }
             break;
           case 'UP':
             if (_snake.last < _squareSize) {
-              _snake.add(_snake.last - _squareSize + (_noOfSquares + _squareSize));
+              _snake.add(
+                  _snake.last - _squareSize + (_noOfSquares + _squareSize));
             } else {
               _snake.add(_snake.last - _squareSize);
             }
@@ -104,7 +106,8 @@ class _GamePageState extends State<GamePage> with TickerProviderStateMixin {
           setState(() {
             _hasStarted = !_hasStarted;
           });
-          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => GameOver(score: _playerScore)));
+          Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) => GameOver(score: _playerScore)));
         }
       });
     }
@@ -115,68 +118,83 @@ class _GamePageState extends State<GamePage> with TickerProviderStateMixin {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        title: Text('Score: $_playerScore', style: TextStyle(color: Colors.white, fontSize: 20.0)),
+        title: Text('Score: $_playerScore',
+            style: TextStyle(color: Colors.white, fontSize: 20.0)),
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton.extended(
-        backgroundColor: Colors.redAccent,
-        elevation: 20,
-        label: Text(
-          _hasStarted ? 'Start' : 'Pause',
-          style: TextStyle(),
-        ),
-        onPressed: () {
-          setState(() {
-            if(_hasStarted) {
-              _snakeController.forward();
-            } else {
-              _snakeController.reverse();
-            }
-            _hasStarted = !_hasStarted;
-            _gameStart();
-          });
+          backgroundColor: Colors.redAccent,
+          elevation: 20,
+          label: Text(
+            _hasStarted ? 'Start' : 'Pause',
+            style: TextStyle(),
+          ),
+          onPressed: () {
+            setState(() {
+              if (_hasStarted) {
+                _snakeController.forward();
+              } else {
+                _snakeController.reverse();
+              }
+              _hasStarted = !_hasStarted;
+              _gameStart();
+            });
+          },
+          icon: AnimatedIcon(
+              icon: AnimatedIcons.play_pause, progress: _snakeAnimation)),
+      body: GestureDetector(
+        onVerticalDragUpdate: (drag) {
+          if (drag.delta.dy > 0 && _currentSnakeDirection != 'UP') {
+            _currentSnakeDirection = 'DOWN';
+          } else if (drag.delta.dy < 0 && _currentSnakeDirection != 'DOWN') {
+            _currentSnakeDirection = 'UP';
+          }
         },
-        icon: AnimatedIcon(icon: AnimatedIcons.play_pause, progress: _snakeAnimation)
-      ),
-      body: Center(
-        child: GestureDetector(
-          onVerticalDragUpdate: (drag) {
-            if (drag.delta.dy > 0 && _currentSnakeDirection != 'UP') {
-              _currentSnakeDirection = 'DOWN';
-            } else if (drag.delta.dy < 0 && _currentSnakeDirection != 'DOWN') {
-              _currentSnakeDirection = 'UP';
-            }
-          },
-          onHorizontalDragUpdate: (drag) {
-            if (drag.delta.dx > 0 && _currentSnakeDirection != 'LEFT') {
-              _currentSnakeDirection = 'RIGHT';
-            } else if (drag.delta.dx < 0 && _currentSnakeDirection != 'RIGHT') {
-              _currentSnakeDirection = 'LEFT';
-            }
-          },
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: GridView.builder(
-              itemCount: _squareSize + _noOfSquares,
-              physics: NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: _squareSize),
-              itemBuilder: (BuildContext context, int index) {
-                return Center(
-                  child: Container(
-                    color: Colors.white,
-                    padding: _snake.contains(index) ? EdgeInsets.all(1) : EdgeInsets.all(0),
-                    child: ClipRRect(
-                      borderRadius: index == _snakeFoodPosition || index == _snake.last ? BorderRadius.circular(7) : _snake.contains(index) ? BorderRadius.circular(2.5) : BorderRadius.circular(1),
-                      child: Container(
-                        color: _snake.contains(index) ? Colors.black : index == _snakeFoodPosition ? Colors.green : Colors.blue
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
+        onHorizontalDragUpdate: (drag) {
+          if (drag.delta.dx > 0 && _currentSnakeDirection != 'LEFT') {
+            _currentSnakeDirection = 'RIGHT';
+          } else if (drag.delta.dx < 0 && _currentSnakeDirection != 'RIGHT') {
+            _currentSnakeDirection = 'LEFT';
+          }
+        },
+        child: SizedBox(
+          width:  MediaQuery.of(context).size.width,
+                  height:  MediaQuery.of(context).size.height,
+          child: GridView.builder(
+            itemCount: _squareSize + _noOfSquares,
+            physics: NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: _squareSize),
+            itemBuilder: (BuildContext context, int index) {
+              return Scaffold(
+                backgroundColor: Colors.white,
+                body: Container(
+                  width:  MediaQuery.of(context).size.width,
+                  height:  MediaQuery.of(context).size.height,
+              padding: _snake.contains(index)
+                  ? EdgeInsets.all(1)
+                  : EdgeInsets.all(0),
+              child: ClipRRect(
+                borderRadius:
+                    index == _snakeFoodPosition || index == _snake.last
+                        ? BorderRadius.circular(10)
+                        : _snake.contains(index)
+                            ? BorderRadius.circular(2.5)
+                            : BorderRadius.circular(1),
+                child: Container(
+                  width:  MediaQuery.of(context).size.width,
+                  height:  MediaQuery.of(context).size.height,
+                    color: _snake.contains(index)
+                        ? Colors.black
+                        : index == _snakeFoodPosition
+                            ? Colors.yellow
+                            : Colors.redAccent),
+              ),
+                ),
+              );
+            },
           ),
         ),
       ),
--- lib/over.dart
+++ lib/over.dart
@@ -15,33 +15,35 @@ class GameOver extends StatelessWidget {
         body: Container(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
-      color: Colors.blue,
+      color: Colors.redAccent,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Text('Game Over',
               style: TextStyle(
-                  color: Colors.redAccent,
+                  color: Colors.black,
                   fontSize: 50.0,
+                  fontStyle:  FontStyle.italic,
                   fontWeight: FontWeight.bold,)),
           SizedBox(height: 50.0),
           Text('Your Score is: $score',
-              style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold)),
+              style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold)),
           SizedBox(height: 50.0),
           ElevatedButton.icon(
               style: ElevatedButton.styleFrom(
+                elevation: 0,
                 padding: EdgeInsets.all(10),
-                  backgroundColor: Colors.redAccent,
+                  backgroundColor: Colors.white,
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(30.0))),
               onPressed: () {
                 Navigator.of(context).pushReplacement(
                     MaterialPageRoute(builder: (context) => GamePage()));
               },
-              icon: Icon(Icons.refresh, color: Colors.white, size: 30.0),
+              icon: Icon(Icons.refresh, color: Colors.redAccent, size: 30.0),
               label: Text("Try Again",
-                  style: TextStyle(color: Colors.white, fontSize: 20.0))),
+                  style: TextStyle(color: Colors.redAccent, fontSize: 20.0))),
         ],
       ),
     ));
